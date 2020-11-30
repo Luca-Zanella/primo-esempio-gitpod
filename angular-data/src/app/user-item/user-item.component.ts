cdb832ff1a2cc38665b7bb4269ed302a1aabc355
Zanella Luca <zanella.luca@iisgalvanimi.edu.it>
2020-11-30 10:20:34 +0100

    0.2.1

--- angular-data/src/app/user-item/user-item.component.ts
+++ angular-data/src/app/user-item/user-item.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
   templateUrl: './user-item.component.html',
-  styleUrls: ['./user-item.component.css']
+  styleUrls: ['./user-item.component.css'],
 })
 export class UserItemComponent implements OnInit {
+  @Input() name: string; //input poi verrà ripreso dentro l'html dove volgiamo
+  
+  constructor() {}
 
-  name: string; // aggiunta la proprietà "name"
-  constructor() {
-    this.name = "Felipe"; //assegnarà name "Felipe" a tutte le istanze
-  }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
