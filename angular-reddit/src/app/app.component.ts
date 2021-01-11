e1afaa364449cbe6c73110ad7332585f5bf198b5
Zanella Luca <zanella.luca@iisgalvanimi.edu.it>
2021-01-11 09:42:40 +0100

    Mxx  es 04.1

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+//il nostro metodo lo andiamo a scrivere dentro l'export
 export class AppComponent {
-  title = 'angular-reddit';
+  //la variabile titile avrà inputElement quindi vuol dire che riceverà qualcosa al suo interno
+  //la stessa cosa per la variabile link che riceverà qualcosa al suo interno come se fosse la plai text su android studio
+  //${titile.value è un modo per inserire direttamente un valore all'interno di una riga}
+  
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(
+      `Adding article title: ${title.value} and link: ${link.value} `
+    );
+    return false;
+  }
 }
